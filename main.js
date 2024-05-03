545301c8e51d41f7cd4e47f0ab268dbd2bc78c51
Youssef Al-Kobary <168835912+youssef1alkobary@users.noreply.github.com>
2024-05-03 16:18:45 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+/*
+// Api
+const myJosnApiProject = `{Username: "Youssef Alkobary", Age: "16"}`;
+console.log(typeof myJosnApiProject);
+console.log(myJosnApiProject);*/
+
+// function getData() {
+//     return new Promise((res, rej) => {
+//         let users = ["Youssef", "Hayder"];
+//         if (users.length > 0) {
+//             res("Users Found");
+//         } else {
+//             rej("Users Not Found");
+//         }
+//     });
+// }
+// getData().then(
+//     (resolvedValue) => console.log(resolvedValue),
+//     (rejectedValue) => console.log(rejectedValue)
+// );
+
